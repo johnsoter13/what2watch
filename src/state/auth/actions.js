45f3f56e027814baa40442107b5c6a191cbc2392
Seung Lee <shl7@uw.edu>
2020-12-20 15:21:51 -0800

    added user streaming service save and load

--- src/state/auth/actions.js
+++ src/state/auth/actions.js
@@ -3,13 +3,17 @@ import { FAILURE, SUCCESS } from '../constants';
 import { SET_LOGIN_STATE } from './constants';
 import {
   createUserWithEmailAndPassword,
+  fetchUserDatabase,
   loginUserWithEmailandPassword,
 } from '../../lib/sdk';
+import { Firebase, db } from '../../../config/Firebase';
+import { UPDATE_STREAMING_SERVICES } from '../streaming/constants';
 
-const loginPayload = (isLoggedIn, uid = '') => {
+const loginPayload = (isLoggedIn, uid = '', idToken = '') => {
   return {
     isLoggedIn,
     uid,
+    idToken,
   };
 };
 
@@ -24,11 +28,14 @@ export const updateUserLogin = (isLoggedIn) => (dispatch) => {
 export const createUserAction = (email, password) => (dispatch) => {
   return createUserWithEmailAndPassword(email, password)
     .then((account) => {
-      // console.log(account);
-      dispatch({
-        type: SET_LOGIN_STATE,
-        status: SUCCESS,
-        payload: loginPayload(true, account.user.uid),
+      account.user.getIdToken().then((idToken) => {
+        // console.log(account);
+        const uid = account.user.uid;
+        dispatch({
+          type: SET_LOGIN_STATE,
+          status: SUCCESS,
+          payload: loginPayload(true, uid, idToken),
+        });
       });
     })
     .catch((error) => {
@@ -51,14 +58,29 @@ export const logUserOutAction = () => (dispatch) => {
 export const loginUserAction = (email, password) => (dispatch) => {
   return loginUserWithEmailandPassword(email, password)
     .then((account) => {
-      // console.log(account.user.uid);
-      dispatch({
-        type: SET_LOGIN_STATE,
-        status: SUCCESS,
-        payload: loginPayload(true, account.user.uid),
+      account.user.getIdToken().then((idToken) => {
+        // console.log(idToken);
+        const { uid } = account.user;
+        fetchUserDatabase(uid)
+          .once('value')
+          .then((snapshot) => {
+            const streamingServices = snapshot.val().streaming_services;
+            dispatch({
+              type: UPDATE_STREAMING_SERVICES,
+              status: SUCCESS,
+              payload: { streamingServices },
+            });
+          });
+
+        dispatch({
+          type: SET_LOGIN_STATE,
+          status: SUCCESS,
+          payload: loginPayload(true, account.user.uid, idToken),
+        });
       });
     })
     .catch((error) => {
+      console.log(error);
       dispatch({
         type: SET_LOGIN_STATE,
         status: FAILURE,
