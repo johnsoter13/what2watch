f77ff62e2383564335b273e6ad323c49a62a1478
John Soter <john.soter@outlook.com>
2020-12-20 16:19:29 -0800

    swiping

--- src/components/SwipeMovieCard/index.js
+++ src/components/SwipeMovieCard/index.js
@@ -1,56 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
 import { StyleSheet, View, Button, Text, Image, Linking } from 'react-native';
 
-import {
-  selectMovieStreamingServicesById,
-  selectMovieStreamingServicesLoadingStatus,
-  selectMovieIdByIndex,
-} from '../../state/movies/selectors';
-import {
-  movieListIndexAction,
-  fetchMovieStreamingServicesAction,
-} from '../../state/movies/actions';
-import { checkIfMovieIsAvailableToUser } from '../../utils/moviesUtils';
-import { SUCCESS } from '../../state/constants';
-
 const SwipeMovieCard = ({ movie, sharedServices }) => {
-  const dispatch = useDispatch();
-  // const movieId = useSelector((state) =>
-  //   selectMovieIdByIndex(state, genre, movieIndex)
-  // );
-  // const [sharedServices, setSharedServices] = useState([]);
-  // const movie = useSelector((state) =>
-  //   selectMovieStreamingServicesById(state, movieId)
-  // );
-
-  // useEffect(() => {
-  //   if (!movieId) {
-  //     return;
-  //   }
-  //   // if not in store, fetch movie
-  //   if (!movie) {
-  //     dispatch(fetchMovieStreamingServicesAction(movieId));
-  //     // sometimes endpoint errors, skip to next movie
-  //   } else if (movie === 'not available') {
-  //     dispatch(movieListIndexAction(genre));
-  //     // check if we have shared streaming services
-  //   } else {
-  //     const sharedServicesForMovie = checkIfMovieIsAvailableToUser(
-  //       userStreamingServices,
-  //       movie
-  //     );
-
-  //     // if yes, set shared services
-  //     if (sharedServicesForMovie.length) {
-  //       setSharedServices(sharedServicesForMovie);
-  //       // skip to next movie
-  //     } else {
-  //       dispatch(movieListIndexAction(genre));
-  //     }
-  //   }
-  // }, [movie, movieId, dispatch, fetchMovieStreamingServicesAction]);
-
   const handleNavigateToLink = (url) => {
     Linking.canOpenURL(url).then((supported) => {
       if (supported) {
